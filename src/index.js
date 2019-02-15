9a05347260bf2cc1b7521050dae3220df7b90380
Didier Stockmans <didier@Didiers-MacBook-Pro.local>
2019-02-15 22:11:30 +0100

    Initial commit.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useContext } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GreetContext = React.createContext();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Greetings = () => {
+  const value = useContext(GreetContext);
+  return <div>Hello {value}, how are you today?</div>;
+};
+
+const App = () => {
+  return (
+    <GreetContext.Provider value={"Didier"}>
+      <Greetings />
+    </GreetContext.Provider>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
